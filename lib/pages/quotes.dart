c6442deaf26ebf07df04204179eeef35f2a409d2
David Li <davidli012345@gmail.com>
2020-05-19 22:11:30 -0700

    Develop (#7)
    * updating CI workflow
    
    * fixing yaml file
    
    * fixing CI pipeline
    
    * testing out fastlane etest
    
    * updating fastlane
    
    * updating run conditions
    
    * fixing deployment issues with fastlane
    
    * fixing app name
    
    * fixing bad build
    
    * Update and rename fastlane_deploy.yml to build_and_deploy.yml
    
    * Update build_and_deploy.yml
    
    * Update build_and_deploy.yml
    
    * adding web support
    
    * updating workflow
    
    * Update build_and_deploy.yml
    
    * Update build_and_deploy.yml
    
    * deleting extra merge artifacts
    
    * Update build_and_deploy.yml
    
    * Update build_and_deploy.yml
    
    * Update build_and_deploy.yml
    
    * adding about us page
    
    * Update main.dart
    
    Uodate
    
    * updating formatting
    
    * adding slide transition
    
    * fixing linting
    
    * updating pages
    
    * importing quotes page
    
    * fixing build
    
    * saving updated formatting
    
    * updating formatting

---
+++ lib/pages/quotes.dart
@@ -0,0 +1,115 @@
+import 'dart:async';
+import 'dart:convert';
+
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+
+Future<Quote> fetchQuote() async {
+  final response = await http.get('https://api.tronalddump.io/random/quote');
+
+  if (response.statusCode == 200) {
+    // If the server did return a 200 OK response,
+    // then parse the JSON.
+    return Quote.fromJson(json.decode(response.body));
+  } else {
+    // If the server did not return a 200 OK response,
+    // then throw an exception.
+    throw Exception('Failed to load album');
+  }
+}
+
+class Quote {
+  final String appearedAt;
+  final String createdAt;
+  final String quoteId;
+  final String updatedAt;
+  final List<String> tags;
+  final String value;
+
+  Quote(
+      {this.appearedAt,
+      this.createdAt,
+      this.quoteId,
+      this.updatedAt,
+      this.tags,
+      this.value});
+
+  factory Quote.fromJson(Map<String, dynamic> json) {
+    return Quote(
+        appearedAt: json['appeared_at'],
+        createdAt: json['created_at'],
+        quoteId: json['quoteId'],
+        updatedAt: json['updated_at'],
+        tags: json['tags'],
+        value: json['value']);
+  }
+}
+
+class QuotePage extends StatefulWidget {
+  // constructor
+  QuotePage({Key key}) : super(key: key);
+  @override
+  _QuotePageState createState() => _QuotePageState();
+}
+
+class _QuotePageState extends State<QuotePage> {
+  Future<Quote> futureQuote;
+
+  @override
+  void initState() {
+    super.initState();
+    futureQuote = fetchQuote();
+  }
+
+  Widget _makeQuote(Quote data) {
+    var quote = new Runes(data.value);
+    String quoteString = new String.fromCharCodes(quote);
+    String createdAt = data.createdAt;
+    return Card(
+        child: new Container(
+            padding: new EdgeInsets.all(32.0),
+            child: ListTile(
+              leading: Icon(
+                Icons.mode_comment,
+                size: 56.0,
+              ),
+              title: Text(quoteString),
+              subtitle: Text(createdAt),
+              trailing: Icon(Icons.more_vert),
+            )));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Get a Quote'),
+      ),
+      body: Center(
+        child: FutureBuilder<Quote>(
+          future: futureQuote,
+          builder: (context, snapshot) {
+            if (snapshot.hasData) {
+              return _makeQuote(snapshot.data);
+            } else if (snapshot.hasError) {
+              return Text("${snapshot.error}");
+            }
+
+            // By default, show a loading spinner.
+            return CircularProgressIndicator();
+          },
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          // Add your onPressed code here!
+          setState(() {
+            futureQuote = fetchQuote();
+          });
+        },
+        child: Icon(Icons.refresh),
+        backgroundColor: Colors.blue,
+      ),
+    );
+  }
+}
